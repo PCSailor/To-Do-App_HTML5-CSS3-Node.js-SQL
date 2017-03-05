51b4f44530dde71a04dc89b27e02b06c59ddddf3
Phil Curtis <Phil@PhilCurtis.io>
2017-03-05 16:46:56 -0600

    previous-code-comment-out_all-working-DOM-DB-DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,32 +2,32 @@ $(function(){
   console.log(new Date().getFullYear() + " jQuery sourced");
 
 
-  // NOTE: ajax.GET
-  getTaskData();
-  function getTaskData() {
-    $.ajax({
-      type: 'GET',
-      url: '/tasks',
-      success: function(response) {
-        console.log('ajax-GET-success-function-response: ', response);
-        $('#tasks_active').empty(); // NOTE: Body ID which clears the list before reloading
-        for (var i = 0; i < response.length; i++) { // NOTE: array of task objects
-          var currentTask = response[i]; // NOTE: Loops through task object
-          var $newTask = $('<tr>'); // NOTE: Creates a new row for each task
-          $newTask.data('id', currentTask.id); // NOTE: Getting data from ?? // QUESTION: Which ID pointting to?
-          $newTask.append('<td><input value=" ' + currentTask.tasks_active + '" class="taskList"></td>');
-          $newTask.append('<td><button class="deleteButton">Delete</button></td>');
-          $newTask.append('<td><button class="editTask">Save Edit</button></td>');
-          $('#tasks_active').prepend($newTask); // NOTE: Add new task to top of list
-          // NOTE: From index.html:
-          // <form id="newTaskForm" action="index.html" method="post">
-          // <input name="tasks_active">
-          // <button type="submit" id="enterNewTaskButton">
-          // <tbody id="tasks_active">
-        } // NOTE: FOR: forLoop
-      } // NOTE: FOR: success: function
-    }); // NOTE: FOR: ajax.GET
-  } // NOTE: FOR: function getTaskData
+  // // NOTE: ajax.GET
+  // getTaskData();
+  // function getTaskData() {
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: '/tasks',
+  //     success: function(response) {
+  //       console.log('ajax-GET-success-function-response: ', response);
+  //       $('#tasks_active').empty(); // NOTE: Body ID which clears the list before reloading
+  //       for (var i = 0; i < response.length; i++) { // NOTE: array of task objects
+  //         var currentTask = response[i]; // NOTE: Loops through task object
+  //         var $newTask = $('<tr>'); // NOTE: Creates a new row for each task
+  //         $newTask.data('id', currentTask.id); // NOTE: Getting data from ?? // QUESTION: Which ID pointting to?
+  //         $newTask.append('<td><input value=" ' + currentTask.tasks_active + '" class="taskList"></td>');
+  //         $newTask.append('<td><button class="deleteButton">Delete</button></td>');
+  //         $newTask.append('<td><button class="editTask">Save Edit</button></td>');
+  //         $('#tasks_active').prepend($newTask); // NOTE: Add new task to top of list
+  //         // NOTE: From index.html:
+  //         // <form id="newTaskForm" action="index.html" method="post">
+  //         // <input name="tasks_active">
+  //         // <button type="submit" id="enterNewTaskButton">
+  //         // <tbody id="tasks_active">
+  //       } // NOTE: FOR: forLoop
+  //     } // NOTE: FOR: success: function
+  //   }); // NOTE: FOR: ajax.GET
+  // } // NOTE: FOR: function getTaskData
 
 
   // NOTE: ajax.POST
@@ -51,43 +51,43 @@ $(function(){
       data: clientObject,  // NOTE: data to routes.js/router.post/var newTasks = req.body;
       success: function(response){
         console.log('client.js/newtask response = ', response);
-        getTaskData();
+        // getTaskData();
         $('#newTaskForm > input').val('');
       } // NOTE: FOR: success-function
     }); // NOTE: FOR: ajax-post
   }); // NOTE: FOR: enterNewTaskButton').on('click'
 
 
-  // NOTE: ajax.delete
-  $('#tasks_active').on('click', '.deleteButton', function(){
-    var idDelectedTask = $(this).parent().parent().data().id; // TODO: CHECK TRANSVERSING
-    console.log('This is the task to delete: ', idDelectedTask);
-    $.ajax({
-      type: 'Delete',
-      url: '/tasks/delete/' + idDelectedTask,
-      success: function(response) {
-        console.log('successful delete reponse: ', reponse);
-        getTaskData();
-      }
-    })
-  });
+  // // NOTE: ajax.delete
+  // $('#tasks_active').on('click', '.deleteButton', function(){
+  //   var idDelectedTask = $(this).parent().parent().data().id; // TODO: CHECK TRANSVERSING
+  //   console.log('This is the task to delete: ', idDelectedTask);
+  //   $.ajax({
+  //     type: 'Delete',
+  //     url: '/tasks/delete/' + idDelectedTask,
+  //     success: function(response) {
+  //       console.log('successful delete reponse: ', reponse);
+  //       getTaskData();
+  //     }
+  //   })
+  // });
 
-
-  // NOTE: ajax.UPDATE
-  $('#tasks_active').on('click', '.editTask', function() {
-    var idEditedTask = $(this).parent().parent().data().id; // TODO: CHECK TRANSVERSING
-    var taskToSave = $(this).parent().parent().find('.taskList').val();
-    var taskObjectToSave = {
-      tasks_active: taskToSave
-    };
-    $.ajax({
-      type: 'PUT',
-      url: '/tasks/edit/' + idEditedTask,
-      data: taskObjectToSave,
-      success: function(response) {
-        console.log('successful update reponse: ', reponse);
-        getTaskData();
-      }
-    });
-  });
+  //
+  // // NOTE: ajax.UPDATE
+  // $('#tasks_active').on('click', '.editTask', function() {
+  //   var idEditedTask = $(this).parent().parent().data().id; // TODO: CHECK TRANSVERSING
+  //   var taskToSave = $(this).parent().parent().find('.taskList').val();
+  //   var taskObjectToSave = {
+  //     tasks_active: taskToSave
+  //   };
+  //   $.ajax({
+  //     type: 'PUT',
+  //     url: '/tasks/edit/' + idEditedTask,
+  //     data: taskObjectToSave,
+  //     success: function(response) {
+  //       console.log('successful update reponse: ', reponse);
+  //       getTaskData();
+  //     }
+  //   });
+  // });
 }); // NOTE: FOR: Doc-ready-$(function(){})
